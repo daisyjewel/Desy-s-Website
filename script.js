5ebae7e5bb6fb7aa158dd00b1607b4c6feea8f69
daisyjewel <desypermatarahman@gmail.com>
2026-08-24 10:31:50 +0700

    hamburger

---
+++ script.js
@@ -0,0 +1,15 @@
+
+const hamburgerBtn= document.getElementById('hamburgerBtn');
+const navMenu = document.getElementById('navMenu');
+
+hamburgerBtn.addEventListener('click', () => {
+  hamburgerBtn.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
+
+document.querySelectorAll('.nav-links a, .btn-hire').forEach(link => {
+link.addEventListener('click',() => {
+hamburgerBtn.classList.remove('active');
+navMenu.classList.remove('active');
+});
+});
